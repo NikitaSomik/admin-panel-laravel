af0b35a3ecf65b9425b0a6e23d07745a6fff27de
Nikita <nikitasomusev@gmail.com>
2019-12-03 00:29:29 +0200

    added changes

--- local.sh
+++ local.sh
@@ -4,6 +4,7 @@ cp -rf ./.env.local ./.env
 
 composer install
 mkdir "./storage/app/public/images"
+#cp ./config/adminlte.local.php ./config/adminlte.php
 php artisan key:generate
 php artisan storage:link
 php artisan migrate --seed
