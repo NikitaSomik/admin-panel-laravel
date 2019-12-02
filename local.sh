d2e1c09f305f1de3ab96619299c6d3634232e194
Nikita <nikitasomusev@gmail.com>
2019-12-03 00:56:43 +0200

    commented command because their need used once

--- local.sh
+++ local.sh
@@ -4,8 +4,8 @@ cp -rf ./.env.local ./.env
 
 composer install
 mkdir "./storage/app/public/images"
-php artisan adminlte:update
-cp ./config/adminlte.local.php ./config/adminlte.php
+#php artisan adminlte:update
+#cp ./config/adminlte.local.php ./config/adminlte.php
 php artisan key:generate
 php artisan storage:link
 php artisan migrate --seed
