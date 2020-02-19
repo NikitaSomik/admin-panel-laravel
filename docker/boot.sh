d73fd5396824f773e02991a5a26e2370c8593d2a
Nikita <nikitasomusev@gmail.com>
2020-02-19 17:43:00 +0200

    docker

---
+++ docker/boot.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+set -e
+
+file_env() {
+  local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+	  echo "error: Both $var and $fileVar are set (but are exclusive)"
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
+
+# Loads various settings that are used elsewhere in the script
+# This should be called after mysql_check_config, but before any other functions
+docker_setup_env() {
+	# Initialize values that might be stored in a file
+	file_env 'DB_DATABASE_MANAGEMENT'
+	file_env 'DB_USERNAME_MANAGEMENT'
+	file_env 'DB_PASSWORD_MANAGEMENT'
+	file_env 'DATAPORTEN_KEY'
+	file_env 'DATAPORTEN_SECRET'
+}
+
+docker_setup_env "$@"
+
+chown -R :www-data storage bootstrap/cache
+chmod -R g+w storage bootstrap/cache
+
+composer install --no-scripts --no-autoloader --ansi --no-interaction -d /var/www/back
+composer dump-autoload -d /var/www/back
+
+if [ ! -f ".env" ]
+then
+  cp .env.example .env
+fi
+
+cd /var/www/back && php artisan key:generate && php artisan migrate --seed && php artisan passport:install
+
+chown -R :www-data storage bootstrap/cache
+chmod -R g+w storage bootstrap/cache
+
+#TODO this chgrp is bad thing.
+cd public
+if [ ! -d "uploads" ]
+then
+  mkdir "uploads"
+fi
+
+chmod -R ug+rwx uploads
+chown -R :www-data uploads
+
+php-fpm
--- start_local.sh
+++ start_local.sh
@@ -2,7 +2,7 @@
 
 cp -rf ./.env.local ./.env
 cd docker
-cp -rf ./.env.local ./.env
+cp -rf ./env-local ./.env
 docker-compose down
 docker-compose --build --no-cache nginx php-fpm mysql
 docker-compose up -d nginx php-fpm mysql
